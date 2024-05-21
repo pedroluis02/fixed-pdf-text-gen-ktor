e73cf47f1fae5087b25c69ff6c0057ce25bb91e9
Pedro Luis <pedroluisnr22@gmail.com>
2024-05-20 22:54:03 -0500

    feat: :sparkles: add unit measure
    Default, inches, centimeters and millimeters measure

---
+++ src/main/kotlin/com/github/pedroluis02/fixedpdfgen/writer/UnitMeasure.kt
@@ -0,0 +1,20 @@
+package com.github.pedroluis02.fixedpdfgen.writer
+
+import com.lowagie.text.Utilities
+
+enum class UnitMeasure {
+    DEFAULT {
+        override fun compute(value: Float) = value
+    },
+    INCHES {
+        override fun compute(value: Float) = Utilities.inchesToPoints(value)
+    },
+    CENTIMETERS {
+        override fun compute(value: Float) = MILLIMETERS.compute(10f * value)
+    },
+    MILLIMETERS {
+        override fun compute(value: Float) = Utilities.millimetersToPoints(value)
+    };
+
+    abstract fun compute(value: Float): Float
+}
