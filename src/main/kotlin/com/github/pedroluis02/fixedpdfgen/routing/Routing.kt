529af2105366578bf3f9b7baa83422fbaa736e78
Pedro Luis <pedroluisnr22@gmail.com>
2024-05-24 01:45:02 -0500

    feat: :sparkles: add pdf generator service and routing

--- src/main/kotlin/com/github/pedroluis02/fixedpdfgen/routing/Routing.kt
+++ src/main/kotlin/com/github/pedroluis02/fixedpdfgen/routing/Routing.kt
@@ -1,13 +1,39 @@
 package com.github.pedroluis02.fixedpdfgen.routing
 
+import com.github.pedroluis02.fixedpdfgen.service.PdfGeneratorSampleService
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/pdf-sample") {
+            val file = File("sample-pdf-${System.currentTimeMillis()}.pdf")
+            try {
+                val generator = PdfGeneratorSampleService()
+                generator.generateFile(file.path)
+
+                call.response.header(
+                    HttpHeaders.ContentDisposition,
+                    ContentDisposition.Inline.withParameter(
+                        ContentDisposition.Parameters.FileName, file.name
+                    ).toString()
+                )
+
+                call.respondFile(file)
+            } catch (ex: Exception) {
+                call.respondText(ex.message ?: "", status = HttpStatusCode.InternalServerError)
+            } finally {
+                if (file.exists()) {
+                    file.delete()
+                }
+            }
         }
     }
 }
