52d17699e04feac83f628ac89437c20aa38f2447
Pedro Luis <pedroluisnr22@gmail.com>
2024-05-30 14:39:26 -0500

    feat: :sparkles: create route for pdf gneration by template

--- src/main/kotlin/com/github/pedroluis02/fixedpdfgen/routing/Routing.kt
+++ src/main/kotlin/com/github/pedroluis02/fixedpdfgen/routing/Routing.kt
@@ -1,8 +1,11 @@
 package com.github.pedroluis02.fixedpdfgen.routing
 
 import com.github.pedroluis02.fixedpdfgen.service.PdfGeneratorSampleService
+import com.github.pedroluis02.fixedpdfgen.service.PdfTemplateGeneratorService
+import com.github.pedroluis02.fixedpdfgen.template.FixedPdfTemplateModel
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
@@ -16,6 +19,12 @@ fun Application.configureRouting() {
         get("/pdf-sample") {
             val file = File("sample-pdf-${System.currentTimeMillis()}.pdf")
             respondInlineFile(call) { PdfGeneratorSampleService().generateFile(file.path) }
+        }
+
+        post("/pdf-template") {
+            val model = call.receive<FixedPdfTemplateModel>()
+            val file = File("fixed-pdf-${System.currentTimeMillis()}.pdf")
+            respondInlineFile(call) { PdfTemplateGeneratorService(model).generateFile(file.path) }
         }
     }
 }
